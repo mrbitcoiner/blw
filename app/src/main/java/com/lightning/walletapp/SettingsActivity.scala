a46858d8c5d935cb50769f771c8c290ed90d0d1d
_ <_>
2025-01-04 00:20:54 +0000

    * Bump version to v0.5.0

--- app/src/main/java/com/lightning/walletapp/SettingsActivity.scala
+++ app/src/main/java/com/lightning/walletapp/SettingsActivity.scala
@@ -113,7 +113,7 @@ class SettingsActivity extends TimerActivity with HumanTimeDisplay { me =>
   def INIT(s: Bundle) = if (app.isAlive) {
     me setContentView R.layout.activity_settings
     me initToolbar findViewById(R.id.toolbar).asInstanceOf[Toolbar]
-    getSupportActionBar setSubtitle "App version 0.4.12"
+    getSupportActionBar setSubtitle "App version 0.5.0"
     getSupportActionBar setTitle wallet_settings
 
     updateConstrainLNFeesView
